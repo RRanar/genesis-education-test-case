4ea454b90bb5981fabd0dfb7a686ca1f5050a0c0
Dmitro <dmitro.terentiuk@gmail.com>
2023-05-28 23:49:18 +0300

    Add missing files

--- Dockerfile
+++ Dockerfile
@@ -2,17 +2,17 @@ FROM golang:1.17-alpine as build-stage
 
 RUN apk --no-cache add ca-certificates
 
-WORKDIR /go/src/github.com/swayne275/joke-web-server
+WORKDIR /go/src/github.com/RRanar/genesis-education-test-case
 
 COPY . .
 
-RUN CGO_ENABLED=0 GOOS=linux go build -a -o /joke-web-server .
+RUN CGO_ENABLED=0 GOOS=linux go build -a -o /genesis-education-test-case .
 
 FROM scratch
 
 COPY --from=build-stage /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/ca-certificates.crt
-COPY --from=build-stage /joke-web-server /joke-web-server
+COPY --from=build-stage /genesis-education-test-case /genesis-education-test-case
 
 EXPOSE 5000
 
-ENTRYPOINT ["/joke-web-server"]
+ENTRYPOINT ["/genesis-education-test-case"]
--- main.go
+++ main.go
@@ -2,8 +2,51 @@ package main
 
 import (
 	"fmt"
+	"genesis-education-test-case/core/routes"
+	emailsender "genesis-education-test-case/core/services/email_sender"
+	"os"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello world")
+	err := godotenv.Load()
+
+	if err != nil {
+	  fmt.Println("Error loading .env file")
+	}
+
+	app := gin.Default()
+	f, err := os.OpenFile(os.Getenv("STORAGE_FILE"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	if err != nil {
+		fmt.Println("Couldn't start server")
+		return
+	}
+
+	f.Close()
+	smtpPort, err := strconv.Atoi(os.Getenv("SMTP_PORT"))
+
+	if err != nil {
+		fmt.Println("Cannot convert \"SMTP_PORT\" to number")
+		return
+	}
+
+	routes.Subscribe(
+		&app.RouterGroup,
+		f,
+		&emailsender.EmailSenderConfig{
+			SmtpUser: os.Getenv("SMTP_USERNAME"),
+			SmtpPass: os.Getenv("SMTP_PASSWORD"),
+			SmtpSender: os.Getenv("SMTP_SENDER"),
+			SmtpHost: os.Getenv("SMTP_HOST"),
+			SmtpPort: int16(smtpPort),
+		},
+		os.Getenv("EXCHANGE_API_KEY"),
+	)
+	routes.Rate(&app.RouterGroup, os.Getenv("EXCHANGE_API_KEY"))
+
+	app.Run("localhost:" + string(os.Getenv("PORT")))
 }
